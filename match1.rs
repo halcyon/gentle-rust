2da6fd2005a6a5bc11116e007d4bd117a25323d4
Scott McLeod <halcyonblue@gmail.com>
2018-01-18 00:51:22 -0500

    add ranges

--- match1.rs
+++ match1.rs
@@ -1,4 +1,4 @@
-// string3.rs
+// match1.rs
 fn main() {
     let multilingual = "Hi! ¡Hola! привет!";
     for ch in multilingual.chars() {
@@ -16,4 +16,19 @@ fn main() {
         },
         None => println!("couldn't find the greeting, Товарищ")
     };
+
+    if let Some(idx) = multilingual.find('п') {
+        println!("Russian hi {}", &multilingual[idx..]);
+    }
+
+    let n = 4;
+    let text = match n {
+        0 => "zero",
+        1 => "one",
+        2 => "two",
+        3...6 => "small",
+        8...10 => "medium",
+        _ => "many",
+    };
+    println!("{}", text);
 }
