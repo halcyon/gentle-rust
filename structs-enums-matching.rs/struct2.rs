01c5937b37e51a63b26cd8598956f0649a859cce
Scott McLeod <halcyonblue@gmail.com>
2018-01-18 23:36:25 -0500

    struct2.rs

--- structs-enums-matching.rs/struct2.rs
+++ structs-enums-matching.rs/struct2.rs
@@ -1,5 +1,6 @@
 // struct2.rs
 
+#[derive(Debug)]
 struct Person {
     first_name: String,
     last_name: String
@@ -12,9 +13,31 @@ impl Person {
             last_name: name.to_string()
         }
     }
+
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+
+    fn copy(&self) -> Self {
+        Self::new(&self.first_name,&self.last_name)
+    }
+
+    fn set_first_name(&mut self, name: &str) {
+        self.first_name = name.to_string();
+    }
+
+    fn to_tuple(self) -> (String,String) {
+        (self.first_name, self.last_name)
+    }
 }
 
 fn main() {
     let p = Person::new("John","Smith");
+    let mut q = p.copy();
+    q.set_first_name("Bob");
     println!("person {} {}", p.first_name,p.last_name);
+    println!("fullname {}", p.full_name());
+    println!("fullname {}", q.full_name());
+    println!("{:?}", q.to_tuple());
+    println!("{:?}", p);
 }
